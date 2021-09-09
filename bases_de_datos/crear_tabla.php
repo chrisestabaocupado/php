bdf1bcdc9cdb5c0def5780f293036d7405e85093
Christopher <christopherglood@gmail.com>
2021-09-09 16:19:22 -0400

    New branch

--- bases_de_datos/crear_tabla.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-include("conexion.php");
-
-$solicitud = "CREATE TABLE datos (id int NOT NULL AUTO_INCREMENT, nombre varchar(15), apellido varchar(15), edad int, celular int, PRIMARY KEY(id))";
-
-$resultado = mysqli_query($conexion ,$solicitud);
-
-?>
--- sesiones/uno.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-session_start();
-echo "Tu nombre es: ".$_SESSION['nombre'];
-
-?>
